d9a352714b08da3107e22f4d992a37d683c522e0
James Clarke <jkc200013@gmail.com>
2019-08-08 21:05:50 +0100

    added in-code exception throwing, and IndexOutOfRangeException

--- framework/core/Exception.cs
+++ framework/core/Exception.cs
@@ -1,4 +1,28 @@
-﻿public class WavyException : System.Exception
+﻿public class ExceptionManager
+{
+    // For use when we want to cause an interrupt from the wavy~ core, such as IndexOutOfBounds
+    public static void interrupt_wavy_exception(Interpreter interpreter, string exception_name)
+    {
+        // First check if the exception is valid
+        WavyClass exception = (WavyClass)interpreter.namespaces["exception"].get("Exception");
+        // Create a new instance of the exception class
+        WavyObject exception_object = (WavyObject)((Callable)interpreter.namespaces["exception"].get(exception_name)).call(interpreter, new System.Collections.Generic.List<object>());
+        // First check if it extends 'Exception'
+        WavyClass super = exception_object.the_class;
+        while (super != exception && super != null)
+        {
+            super = super.superclass;
+        }
+        if (super != exception)
+        {
+            throw new RuntimeException("Class must be an Exception to be able to interrupt");
+        }
+        // Then throw the interrupt
+        throw new InterruptException(null, (string)exception_object.get("message"));
+    }
+}
+
+public class WavyException : System.Exception
 {
     public string errror_msg;
 }
