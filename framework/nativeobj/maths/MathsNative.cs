38a5cf156ae13fae9100337240be09ed3dc84ed7
james clarke <jkc200013@gmail.com>
2019-08-10 22:56:41 +0100

    added random number ranges

--- framework/nativeobj/maths/MathsNative.cs
+++ framework/nativeobj/maths/MathsNative.cs
@@ -5,6 +5,8 @@ public static class MathsNative
 {
     public static Dictionary<string, object> native_obj = new Dictionary<string, object>()
     {
+        
+        { "rand_native", NativeLoader.create_native(2, (interpreter, args) => new Random().NextDouble()) },
         { "power_native", NativeLoader.create_native(2, (interpreter, args) => Math.Pow(Convert.ToDouble(args[1]), Convert.ToDouble(args[0]))) },
         { "log_native", NativeLoader.create_native(2, (interpreter, args) => Math.Log(Convert.ToDouble(args[1]), Convert.ToDouble(args[0]))) },
     };
